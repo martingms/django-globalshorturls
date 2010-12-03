6d29bee1e10a74ba1b2836ce61523cfffee11f15
martingamm <martigam@stud.ntnu.no>
2010-12-03 15:01:57 +0100

    Removed unused imports in views.py and fixed some indentation

--- globalshorturls/views.py
+++ globalshorturls/views.py
@@ -1,9 +1,7 @@
 # encoding: utf-8
-#FIXME all imports might not be in use
 from django.shortcuts import render_to_response, get_object_or_404
-from django import forms
 from django.http import HttpResponseRedirect, Http404
-from django.template import Context, loader, RequestContext
+from django.template import RequestContext
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
 from django.core.urlresolvers import reverse
@@ -11,23 +9,23 @@ from django.core.urlresolvers import reverse
 from globalshorturls.models import Shorturl, ShorturlForm
 from globalshorturls.baseconv import base62
 
-@login_required 
+@login_required
 def index(request):
     '''
-    	View that gives you a prompt for shortening an URL, and shows the users shortened urls.
+        View that gives you a prompt for shortening an URL, and shows the users shortened urls.
     '''
     user = request.user
     user_shorturls = Shorturl.objects.filter(creator=user)
-    
+
     if request.method == "POST":
          shorturlform = ShorturlForm(request.POST)
          if shorturlform.is_valid():
- 	        shorturl = Shorturl(url=shorturlform.cleaned_data['url'], creator = user)
- 	        #TODO look for a better way to create shorturl from id than saving twice
- 	        shorturl.save()
- 	        shorturl.shorturl = base62.from_decimal(shorturl.id)
- 	        shorturl.save()
- 	        
+            shorturl = Shorturl(url=shorturlform.cleaned_data['url'], creator = user)
+            #TODO look for a better way to create shorturl from id than saving twice
+            shorturl.save()
+            shorturl.shorturl = base62.from_decimal(shorturl.id)
+            shorturl.save()
+
     shorturlform = ShorturlForm()
 
     return render_to_response('globalshorturls/index.html',
@@ -36,7 +34,7 @@ def index(request):
                           # User is not used in the example index.html template, but might be useful
                           'user'           : user, },
                           context_instance=RequestContext(request))
-                          
+
 def redirect(request, shorturl):
     '''
         Redirects a yourdomain.com/XYZ shorturl to the full url
@@ -48,19 +46,19 @@ def redirect(request, shorturl):
         url.save()
         return HttpResponseRedirect(url.url)
     except:
-	    raise Http404
-	    
+        raise Http404
+
 @login_required
 def delete_shorturl(request, url_id):
     '''
         Deletes the shorturl with id url_id
     '''
     shorturl = get_object_or_404(Shorturl, pk=url_id)
-    
+
     if shorturl.creator == request.user:
         shorturl.delete()
         # You might want some kind of message saying success here
         return HttpResponseRedirect(reverse('globalshorturls.index'))
- 
+
     # You might want some kind of message saying you can't delete other peoples urls here
     return HttpResponseRedirect(reverse('globalshorturls.index'))
