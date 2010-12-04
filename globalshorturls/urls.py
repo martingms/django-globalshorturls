f20c7029cad447c26e713e81bf1a49378c22004d
martingamm <martigam@stud.ntnu.no>
2010-12-04 14:42:02 +0100

    Updated the readme with some usage instructions. Clarified a comment in urls.py

--- globalshorturls/urls.py
+++ globalshorturls/urls.py
@@ -4,6 +4,6 @@ from django.conf.urls.defaults import *
 urlpatterns = patterns('globalshorturls.views',
     url(r'^$', 'index', name='globalshorturls.index'),
     url(r'^deleteshorturl/(?P<url_id>\d+)/$', 'delete_shorturl', name='globalshorturls.delete_shorturl'),
-    # You may want to move the following to the end of the global urls.py
+    # You may want to move the following to the end of the global urls.py depending on how you manage your urls
     url(r'^(?P<shorturl>[a-zA-Z0-9]+)/$', 'globalshorturls.views.redirect', name='globalshorturls.redirect')
 )
