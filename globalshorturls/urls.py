e46fdafd37836a14c39fd7ddc4f56beb7d5d62a0
martingamm <martigam@stud.ntnu.no>
2010-12-08 18:58:06 +0100

    Small change in urls.py so that the app works even if you don't move the url for redirect to the global urls.py

--- globalshorturls/urls.py
+++ globalshorturls/urls.py
@@ -4,6 +4,9 @@ from django.conf.urls.defaults import *
 urlpatterns = patterns('globalshorturls.views',
     url(r'^$', 'index', name='globalshorturls.index'),
     url(r'^deleteshorturl/(?P<url_id>\d+)/$', 'delete_shorturl', name='globalshorturls.delete_shorturl'),
-    # You may want to move the following to the end of the global urls.py depending on how you manage your urls
+)
+
+# You probably want to move this addition to the urlpatterns to your global urls.py
+urlpatterns += patterns('',
     url(r'^(?P<shorturl>[a-zA-Z0-9]+)/$', 'globalshorturls.views.redirect', name='globalshorturls.redirect')
 )
